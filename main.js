b8b6ad5ee020eb0dd036c160067ba01fd90ee006
Henry Tseng <henryiis@gmail.com>
2018-08-24 11:30:11 -0400

    Update

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 
 function main() {
   // Get A WebGL context
-  var canvas = document.getElementById("c");
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.getElementById("scene");
   var gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
-  // Use our boilerplate utils to compile the shaders and link into a program
+  // setup GLSL program
   var program = webglUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
 
   // look up where the vertex data needs to go.
@@ -16,22 +17,13 @@ function main() {
 
   // look up uniform locations
   var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
 
   // Create a buffer to put three 2d clip space points in
   var positionBuffer = gl.createBuffer();
 
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  var positions = [
-    10, 20,
-    80, 20,
-    10, 30,
-    10, 30,
-    80, 20,
-    80, 30,
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
@@ -63,11 +55,47 @@ function main() {
   // set the resolution
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-  // draw
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 6;
-  gl.drawArrays(primitiveType, offset, count);
+  // draw 50 random rectangles in random colors
+  for (var ii = 0; ii < 50; ++ii) {
+    // Setup a random rectangle
+    // This will write to positionBuffer because
+    // its the last thing we bound on the ARRAY_BUFFER
+    // bind point
+    var x = randomInt(300);
+    var y = randomInt(300);
+    setRectangle(
+        gl, x, y, 10, 10);
+
+    // Set a random color.
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+    // Draw the rectangle.
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+  }
+}
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+// Fill the buffer with the values that define a rectangle.
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), gl.STATIC_DRAW);
 }
 
 main();
